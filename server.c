a65e132554a8f7d7431def0a8254e30e9aa8916a
Robin Delaporte <robin-delaporte@hotmail.fr>
2016-10-26 13:22:33 +0200

    Mise à jour script server

--- server.c
+++ server.c
@@ -2,12 +2,15 @@
 Serveur à lancer avant le client
 ------------------------------------------------*/
 #include <stdlib.h> 
+#include <stdbool.h> 
 #include <stdio.h> 
 #include <linux/types.h> /* pour les sockets */ 
 #include <sys/socket.h> 
 #include <netdb.h> /* pour hostent, servent */ 
 #include <string.h> /* pour bcopy, ... */ 
 #include <pthread.h> 
+#include <time.h>
+
 
 #define TAILLE_MAX_NOM 256
 
@@ -19,10 +22,10 @@ typedef struct hostent hostent;
 typedef struct servent servent;
 
 typedef struct {
-  int id;
-  char question[256];
-  char rep[256];
-  char sugg[4][256];
+  int id;   //Numéro de quesiton
+  char question[256]; //Question posée
+  char rep[256]; // Réponse correct
+  char sugg[4][256]; //Tableau de toute les suggestions
 }
 Question;
 
@@ -35,48 +38,94 @@ typedef struct {
 }
 Client;
 
-Client threads[TAILLE_MAX_CLIENT];
+Client threads[TAILLE_MAX_CLIENT]; //Tableau de tous les clients
 int socket_descriptor; /* descripteur de socket */
+pthread_t threadCmd; //Thread de commande, permet d'écouter les interactions sur le serveur
+int nbClientCo = 0; 
+Question quizz[255]; //Tableau des questions
 
-pthread_t threadCmd;
 
-int nbClientCo = 0;
-Question quizz[5];
+/********************************************/
+/***********Envoie un message à tous les*****/
+/***********client connectés*****************/
+/********************************************/
 
 int broadCastMessage(char msg[256]) {
   int i = 0;
   int vreturn = 0;
-  for (i; i < sizeof(threads) / sizeof(threads[0]); ++i) {
+  for (i; i < sizeof(threads) / sizeof(threads[0]); ++i) { //Parcours detous les utilisateurs
     if (threads[i].connected == 1) {
       if (vreturn != -1 && write(threads[i].sock, msg, strlen(msg) + 1) < 0) {
         vreturn = -1;
       }
-
     }
   }
-
   return vreturn;
-
 }
 
-  void printQuizz(){
-    int i = 0;
-    int j = 0;
-    for (i; i < sizeof(quizz) / sizeof(quizz[0]); i++) {
-      if (quizz[i].id > 0) {
-        j=0;
-        printf("Question n°%d\n question:%s\n reponses:%s\n sugestions:",quizz[i].id, quizz[i].question, quizz[i].sugg[0]);
-        for (j; j < sizeof(quizz[i].sugg) / sizeof(quizz[i].sugg[0]); j++)
+bool isValueInArray(int val, int *arr, int size){
+    int i;
+    for (i=0; i < size; i++) {
+        if (arr[i] == val)
+            return true;
+    }
+    return false;
+}
+
+/**********************************************/
+/****Retourne le nombre de questions****/
+/*********************************************/
+int countNumberOfQuestions(){
+  int i = 0;
+  int vretour = 0;
+  for (i; i < sizeof(quizz) / sizeof(quizz[0]); i++) {
+    if (quizz[i].id > 0) {
+      vretour++;
+    }
+  }
+  return vretour++;
+}
+
+Question getRandomQuestion(int *questionsAsked, int nbOfQuestionsAsked){
+  Question q = { 0, "", ""};;
+  int r;
+  while(q.id == 0){
+    srand(time(NULL));
+    r = rand() % countNumberOfQuestions();
+    if (!isValueInArray(r + 1, questionsAsked, countNumberOfQuestions()))
+    {
+      q = quizz[r];
+      questionsAsked[nbOfQuestionsAsked + 1] = r + 1;
+    }
+  }
+  return q;
+}
+
+/**********************************************/
+/****Affiche toutes les questions chargées****/
+/*********************************************/
+void printQuizz(){
+  int i = 0;
+  int j = 0;
+  for (i; i < sizeof(quizz) / sizeof(quizz[0]); i++) {
+    if (quizz[i].id > 0) {
+      j=0;
+      printf("Question n°%d\n question:%s\n reponses:%s\n sugestions:",quizz[i].id, quizz[i].question, quizz[i].sugg[0]);
+      for (j; j < sizeof(quizz[i].sugg) / sizeof(quizz[i].sugg[0]); j++)
+      {
+        if (quizz[i].sugg[j] != NULL)
         {
-          if (quizz[i].sugg[j] != NULL)
-          {
-            printf("%s\n", quizz[i].sugg[j]);
-          }
+          printf("%s\n", quizz[i].sugg[j]);
         }
       }
     }
   }
+}
 
+/********************************************************/
+/****Ferme toutes les connexions, et tous les threads****/
+/****Ordonne aux clients de fermer leurs connexions******/
+/********************************************************/
 void closeConnections() {
   int i = 0;
   broadCastMessage("q");
@@ -93,30 +142,9 @@ void closeConnections() {
 
 }
 
-static void * serverAction() {
-  char mesg[256]; /* message envoyé */
-  char retour[256];
-  while (1) {
-    memset(mesg, 0, 255);
-    memset(retour, 0, 255);
-    fgets(mesg, sizeof(mesg), stdin);
-    mesg[strcspn(mesg, "\n")] = '\0';
-    if (mesg[0] == 'q') {
-      closeConnections();
-    }else if (mesg[0] == '1')
-    {
-      printQuizz();
-    } else {
-      sprintf(retour, "Le serveur a dit: %s \n", mesg);
-      if ((broadCastMessage(retour)) < 0) {
-        perror("erreur : impossible d'ecrire le message destine au serveur.");
-        exit(0);
-      }
-    }
-  }
-}
-
-/*------------------------------------------------------*/
+/********************************************************/
+/****Ecoute les messages à reçus par les clients*********/
+/********************************************************/
 static void * newClient(void * s) {
   Client * client = (Client * ) s;
   int sock = ( * client).sock;
@@ -128,57 +156,118 @@ static void * newClient(void * s) {
     memset(retour, 0, 255);
     if ((longueur = read(sock, buffer, sizeof(buffer))) <= 0) {
       printf("message nul. \n");
+      close(sock);
+      return;
+    }
+    if (buffer[0] == 'q') {
+      close(sock);
+      sprintf(retour, "%s a quitté la partie.", ( * client).pseudo);
+      printf("%s\n", retour);
+      broadCastMessage(retour);
       return;
     }
     sprintf(retour, "%s a dit: %s \n", ( * client).pseudo, buffer);
     printf("%s\n", retour);
     broadCastMessage(retour);
-    //write(sock,retour,strlen(retour)+1);
-
   }
   close(sock);
   return;
 
 }
 
-  void readFile(){
-    const char s[2] = "|";
-    int compteur = 1;
-    int i = 0;
-    const char sep[2] = ";";
-    char * reponses;
-    char *token;
-    FILE *file;
-    char * newLine;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    file = fopen("test.txt", "r");
-    if (file == NULL)
-      exit(EXIT_FAILURE);
-    Question q;
-    while ((read = getline(&line, &len, file)) != -1) {
-      i = 0;
-      newLine = line;
-      token = strtok(newLine, s);
-      quizz[compteur -1].id = compteur;
-      sprintf(quizz[compteur - 1].question, "%s", token);
-      token = strtok(NULL, s);
-      token = strtok(token, sep);
-      sprintf(quizz[compteur - 1].rep, "%s", token);
-      while(token!= NULL){
-        sprintf(quizz[compteur - 1].sugg[i], "%s", token);
-        token = strtok(NULL, sep);
-        i++;
-      }
-      compteur++;
+/********************************************************/
+/****Chargement des questions via le fichier*************/
+/********************************************************/
+void readFile(){
+  const char s[2] = "|";
+  int compteur = 1;
+  int i = 0;
+  const char sep[2] = ";";
+  char * reponses;
+  char *token;
+  FILE *file;
+  char * newLine;
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  file = fopen("test.txt", "r");
+  if (file == NULL)
+    exit(EXIT_FAILURE);
+  Question q;
+  while ((read = getline(&line, &len, file)) != -1) {
+    i = 0;
+    newLine = line;
+    token = strtok(newLine, s);
+    quizz[compteur -1].id = compteur;
+    sprintf(quizz[compteur - 1].question, "%s", token);
+    token = strtok(NULL, s);
+    token = strtok(token, sep);
+    sprintf(quizz[compteur - 1].rep, "%s", token);
+    while(token!= NULL){
+      sprintf(quizz[compteur - 1].sugg[i], "%s", token);
+      token = strtok(NULL, sep);
+      i++;
     }
-    fclose(file);
+    compteur++;
   }
+  fclose(file);
+}
 
+void playGame(){
+  int scoreMax = 0;
+  bool hasWinner = false;
+  int questionsAsked[countNumberOfQuestions()];
+  int i = 0;
+  Question currentQuestion;
+  for (i; i < countNumberOfQuestions(); ++i){
+    currentQuestion = getRandomQuestion(questionsAsked, i);
+    printf("%s\n", currentQuestion.question);
+    sleep(2);
+  }
+}
 
+void printMenu(char mesg[256]) {
+  printf(" ___________________Menu serveur__________________ \n");
+  printf("|                                                 |\n");
+  printf("|Afficher les questions chargées_________________1|\n");
+  printf("|Recharger les questions_________________________2|\n");
+  printf("|Commencer le jeu________________________________b|\n");
+  printf("|Quitter_________________________________________q|\n");
+  printf("|                                                 |\n");
+  printf("|_________________________________________________|\n");
+}
 
-/*------------------------------------------------------*/
+/********************************************************/
+/****Ecoute des messages à envoyer par le serveur********/
+/********************************************************/
+static void * serverAction() {
+  char mesg[256]; /* message à envoyé */
+  char retour[256]; 
+  while (1) {
+    memset(mesg, 0, 255);
+    memset(retour, 0, 255);
+    printMenu(mesg);
+    fgets(mesg, sizeof(mesg), stdin);
+    mesg[strcspn(mesg, "\n")] = '\0';
+    switch(mesg[0]) {
+      case '1'  :
+        printQuizz();
+        break; 
+      case 'q'  :
+        closeConnections();
+        break; 
+      case 'b':
+        playGame();
+        break;  
+      default : 
+        sprintf(retour, "Le serveur a dit: %s \n", mesg);
+        if ((broadCastMessage(retour)) < 0) {
+          perror("erreur : impossible d'ecrire le message destine au serveur.");
+          exit(0);
+        }
+    }
+  }
+}
 
 /*------------------------------------------------------*/
 main(int argc, char * * argv) {
@@ -189,49 +278,38 @@ main(int argc, char * * argv) {
   hostent * ptr_hote; /* les infos recuperees sur la machine hote */
   servent * ptr_service; /* les infos recuperees sur le service de la machine */
   char machine[TAILLE_MAX_NOM + 1]; /* nom de la machine locale */
-
   gethostname(machine, TAILLE_MAX_NOM); /* recuperation du nom de la machine */
-
   /* recuperation de la structure d'adresse en utilisant le nom */
   if ((ptr_hote = gethostbyname(machine)) == NULL) {
     perror("erreur : impossible de trouver le serveur a partir de son nom.");
     exit(1);
   }
-
   /* initialisation de la structure adresse_locale avec les infos recuperees */
 
   /* copie de ptr_hote vers adresse_locale */
   bcopy((char *)ptr_hote->h_addr, (char * ) & adresse_locale.sin_addr, ptr_hote->h_length);
   adresse_locale.sin_family = ptr_hote->h_addrtype; /* ou AF_INET */
   adresse_locale.sin_addr.s_addr = INADDR_ANY; /* ou AF_INET */
-
   adresse_locale.sin_port = htons(5000);
-
   printf("numero de port pour la connexion au serveur : %d \n",
     ntohs(adresse_locale.sin_port) /*ntohs(ptr_service->s_port)*/ );
-
   /* creation de la socket */
   if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("erreur : impossible de creer la socket de connexion avec le client.");
     exit(1);
   }
-
   /* association du socket socket_descriptor à la structure d'adresse adresse_locale */
   if ((bind(socket_descriptor, (sockaddr * )( & adresse_locale), sizeof(adresse_locale))) < 0) {
     perror("erreur : impossible de lier la socket a l'adresse de connexion.");
     exit(1);
   }
-
   /* initialisation de la file d'ecoute */
   listen(socket_descriptor, 5);
-
   pthread_create( & threadCmd, NULL, serverAction, NULL);
   readFile();
   /* attente des connexions et traitement des donnees recues */
   while (1) {
-
     longueur_adresse_courante = sizeof(adresse_client_courant);
-
     /* adresse_client_courant sera renseigné par accept via les infos du connect */
     if ((nouv_socket_descriptor =
         accept(socket_descriptor,
@@ -239,24 +317,11 @@ main(int argc, char * * argv) {
       perror("erreur : impossible d'accepter la connexion avec le client.");
       exit(1);
     }
-
     threads[nbClientCo].sock = nouv_socket_descriptor;
     threads[nbClientCo].connected = 1;
     sprintf(threads[nbClientCo].pseudo, "joueur %d", nouv_socket_descriptor);
     printf("Démarrage connection avec un nouveau client \n");
     pthread_create( & threads[nbClientCo].thread, NULL, newClient, & threads[nbClientCo]);
-
     nbClientCo++;
-
   }
-
-}
-
-char * concat(const char * s1,
-  const char * s2) {
-  char * result = malloc(strlen(s1) + strlen(s2) + 1); //+1 for the zero-terminator
-  //in real code you would check for errors in malloc here
-  strcpy(result, s1);
-  strcat(result, s2);
-  return result;
 }
